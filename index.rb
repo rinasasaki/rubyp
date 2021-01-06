06da9f6d98c7492c1bdd08ef26be3708dc96d49b
佐々木　莉奈 <1072sasaki@gmail.com>
2021-01-06 23:20:05 +0900

    Update index.rb index.rbを更新

---
+++ index.rb
@@ -0,0 +1,13 @@
+puts "Hello Ruby"
+#puts "こんにちは"
+puts 2
+puts 2 + 3
+puts 7 - 4
+puts 5 + 2
+puts "5 + 2"
+message="こんにちは"
+puts message + "にんじゃわんこさん"
+name="ひつじ仙人さん"
+puts message + name
+number1=10
+puts number 1 + number 2
